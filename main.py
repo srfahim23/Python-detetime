ee18911a1fd1c5df51acb68fe2439d9f5b0fb60c
Muhammad Faysal <srfahim23@gmail.com>
2023-02-11 18:57:57 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Import the datetime module and display the current date:
+
+# Python Datetime
+# import datetime
+
+# x = datetime.datetime.now()
+# print(x)
+
+
+# Return the year and name of weekday:
+# import datetime
+
+# x = datetime.datetime.now()
+
+# print(x.year)
+# print(x.strftime("%A"))
+
+
+# Create a date object:
+# import datetime
+
+# x = datetime.datetime(2020, 5, 17)
+
+# print(x)
+
+
+#Display the name of the month:
+# import datetime
+# x = datetime.datetime(2018, 6, 1)
+
+# print(x.strftime("%B"))
